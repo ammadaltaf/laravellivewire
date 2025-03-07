8c93d6a9fe9a864d0705f870a20292b0e2cce5cc
Ammad Altaf <ammadaltaf005@gmail.com>
2025-03-07 09:55:27 +0500

    Applied Middleware

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Livewire\Register;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/',Home::class)->name('home');
-Route::get('/login',Login::class)->name('login');
-Route::get('/register',Register::class)->name('register');
+Route::get('/',Home::class)->name('home')->middleware('auth');
+Route::group(['middleware'=>'guest'],function(){
+    Route::get('/login',Login::class)->name('login');
+    Route::get('/register',Register::class)->name('register');
+});
